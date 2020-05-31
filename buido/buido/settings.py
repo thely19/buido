b3f3613661ff9c08bca2d47ff2a64b6926238610
Thely <thelvys80@gmail.com>
2020-05-31 11:53:02 +0200

    modified settings file

--- buido/buido/settings.py
+++ buido/buido/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd%6vcx(gnna%m(i1%q#r%a)8pl@tot6fohuy+co=3m78d^##*g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['buido.herokuapp.com','127.0.0.1']
 
 
 # Application definition
